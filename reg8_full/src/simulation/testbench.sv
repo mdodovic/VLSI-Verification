4942e8daa21494bfd6fa725dc981d058443f69fb
mdodovic <matija.dodovic@gmail.com>
2022-02-09 18:22:56 +0100

    create testbench; dut instance; interface instance

--- reg8_full/src/simulation/testbench.sv
+++ reg8_full/src/simulation/testbench.sv
@@ -14,3 +14,25 @@ interface register_if(
 	logic [7:0] parallel_output;
 
 endinterface //register_if
+
+module testbench;
+    
+    reg clk;
+
+    register_if dut_if (
+        .clk(clk);
+    )
+    
+    dut dut_inst (
+        .clk(clk),
+        .rst_n(dut_if.rst_n),
+        .control(dut_if.control),
+        .serial_input_lsb(dut_if.serial_input_lsb),
+        .serial_input_msb(dut_if.serial_input_msb),
+        .parallel_input(dut_if.parallel_input),
+        .serial_output_lsb(dut_if.serial_output_lsb),
+        .serial_output_msb(dut_if.serial_output_msb),
+        .parallel_output(dut_if.parallel_output)
+    );
+
+endmodule
