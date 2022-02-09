047227d2d0739f7fd2da531d18a3048ba596b29d
mdodovic <matija.dodovic@gmail.com>
2022-02-09 15:32:21 +0100

    add agent (uvm_agent)

--- reg8/src/simulation/testbench_uvm.sv
+++ reg8/src/simulation/testbench_uvm.sv
@@ -123,3 +123,31 @@ class monitor extends uvm_monitor;
 	endfunction
 
 endclass
+
+// Agent
+class agent extends uvm_agent;
+
+	`uvm_component_utils(agent);
+
+	function new(string name = "agent", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction 
+
+	driver d0;
+	monitor m0;
+	uvm_sequencer #(reg8_item) s0;
+
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+
+		d0 = driver::type_id::create("d0", this);
+		m0 = monitor::type_id::create("m0", this);
+		s0 = uvm_sequencer#(reg8_item)::type_id::create("d0", this);
+	endfunction 
+
+	virtual function void connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+		d0.seq_item.port.connect(s0.seq_item_export);
+	endfunction
+
+endclass
