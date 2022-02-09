c47eae7d5163a3c2c6e30d7ec03cbcbb48fb0082
mdodovic <matija.dodovic@gmail.com>
2022-02-09 18:49:29 +0100

    build agent and scoreboard; connect agent.monitor and scoreboard

--- reg8_full/src/simulation/testbench.sv
+++ reg8_full/src/simulation/testbench.sv
@@ -10,6 +10,20 @@ class evn extends uvm_env;
         super.new(name, parent);
     endfunction //new()
     
+    agent a0;
+    scoreboard sb0;
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        a0 = agent::type_id::create("a0", this);
+        sb0 = scoreboard::type_id::create("sb0", this);
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        a0.m0.mon_analysis_port.connect(sb0.mon_analysis_imp);
+    endfunction
+
 endclass //evn
 
 
