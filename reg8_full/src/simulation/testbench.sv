0de98d94ff7b928c3140b6e77d801f97b5de4bed
mdodovic <matija.dodovic@gmail.com>
2022-02-09 18:12:22 +0100

    create interface for dut

--- reg8_full/src/simulation/testbench.sv
+++ reg8_full/src/simulation/testbench.sv
@@ -1,2 +1,16 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
+
+interface register_if(
+    input bit clk
+);
+	logic rst_n;
+	logic [14:0] control;
+	logic serial_input_lsb;
+	logic serial_input_msb;
+	logic [7:0] parallel_input;
+	logic serial_output_lsb;
+	logic serial_output_msb;
+	logic [7:0] parallel_output;
+
+endinterface //register_if
