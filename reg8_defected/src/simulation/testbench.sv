39c94f6a6d5077a5ba5fbc406a24258330b3edf9
mdodovic <matija.dodovic@gmail.com>
2022-02-11 16:59:32 +0100

    create testbench

--- reg8_defected/src/simulation/testbench.sv
+++ reg8_defected/src/simulation/testbench.sv
@@ -287,3 +287,36 @@ interface register_if (
 	logic [7:0] parallel_output;
 
 endinterface //register_if
+
+module testbench;
+    
+    reg clk;
+    
+    register_if dut_if(
+        .clk (clk)
+    );
+
+    register dut_impl(
+        .rst_n (dut_if.rst_n),
+        .control (dut_if.control),
+        .serial_input_lsb (dut_if.serial_input_lsb),
+        .serial_input_msb (dut_if.serial_input_msb),
+        .parallel_input (dut_if.parallel_input),
+        .serial_output_lsb (dut_if.serial_output_lsb),
+        .serial_output_msb (dut_if.serial_output_msb),
+        .parallel_output (dut_if.parallel_output)
+    );
+
+    initial begin
+        clk = 0;
+        forever begin
+            #10; clk = ~clk;
+        end
+    end
+
+    initial begin
+        uvm_config_db#(virtual register_if)::set(null, "*", "register_vif", dut_if);
+        run_test("test");
+    end
+
+endmodule
