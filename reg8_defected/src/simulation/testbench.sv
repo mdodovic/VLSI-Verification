75ee3ebd405ea78c4f04a4687fe64b4f6a71d268
mdodovic <matija.dodovic@gmail.com>
2022-02-11 16:50:52 +0100

    create env class

--- reg8_defected/src/simulation/testbench.sv
+++ reg8_defected/src/simulation/testbench.sv
@@ -212,6 +212,30 @@ class scoreboard extends uvm_scoreboard;
 
 endclass //scoreboard extends uvm_scoreboard
 
+class env extends uvm_env;
+
+    `uvm_component_utils(scoreboard)
+
+    function new(string name = "scoreboard");
+        super.new(name);
+    endfunction //new()
+
+    agent a0;
+    scoreboard sb0;
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        a0 = agent::type_id::create("a0", this);
+        sb0 = scoreboard::type_id::create("sb0", this);
+    endfunction 
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        a0.m0.mon_analysis_port.connect(sb0.mon_analysis_imp);
+    endfunction
+    
+endclass //env extends uvm_env
+
 
 interface register_if (
     input bit clk
